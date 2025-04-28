1810256ff309d9311811ba70a73eec6af2daeb9e
farnaz <farnaz.towhidi@gmail.com>
2025-04-28 14:32:42 -0400

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById("btnSearch").addEventListener ("click",fetchPictures)
 async function fetchPictures () {
 
     let searchTerm = document.getElementById("searchPhrase").value
-    let responseGiphy = await fetch (`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+    let responseGiphy = await fetch (`https://api.giphy.com/v1/gifs/search?api_key=${GIPY_API_KEY}&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
     
     let jsonGiphy = await responseGiphy.json()
     let dataGiphy = jsonGiphy.data
